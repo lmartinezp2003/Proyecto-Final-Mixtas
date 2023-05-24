c79e84f781ab39063ee9fbe17a4df67a2d496081
Laura <l.martinezp@uniandes.edu.co>
2023-05-24 17:52:28 -0500

    Proyecto

---
+++ Assets/Scenes/BatDestroy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BatDestroy : MonoBehaviour {
+
+	public Material capMaterial;
+	public AudioSource source;
+
+	void OnCollisionEnter(Collision collision) {
+		GameObject victim = collision.collider.gameObject;
+
+		GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+		if (!pieces[1].GetComponent<Rigidbody>())
+		{
+			pieces[1].AddComponent<Rigidbody>();
+			MeshCollider temp = pieces[1].AddComponent<MeshCollider>();
+			temp.convex = true; 
+			source.Play();
+		}
+
+	}
+}
